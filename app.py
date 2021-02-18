fd9407e1c68ff9e827ba713de93df19de209cdac
SS-2020 <gsubhasree2@gmail.com>
2021-02-18 14:21:44 +0530

    checks for upload and train

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def append():
             df = pd.read_pickle("uploads//"+db_choices[current_choice]+"-data.pkl")
             df = df.append(sample_data,ignore_index=True)
             df.to_pickle("uploads//"+db_choices[current_choice]+"-data.pkl")
-            rows=len(df)
-            col=len(df.columns)
+        rows=len(df)
+        col=len(df.columns)
     return render_template('index.html',info="Dataset has {r} rows, {c} columns".format(r=rows,c=col),response="Data Successfully added to table", db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
     
 @app.route('/train',methods=['POST'])
@@ -72,54 +72,59 @@ def train():
     model_choice = request.form['model_choice']
     x = df.iloc[:,:-1].values
     y = df.iloc[:,-1].values
-    if model_choice == 'decisiontree':
-        from sklearn import tree
-        classifier=tree.DecisionTreeClassifier()
-        classifier.fit(x,y)
-        pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-dtree.pkl",'wb'))
-        with open("uploads//"+db_choices[current_choice]+"-dtree.pkl", 'rb') as f:
-            data = pickle.load(f)
-        fig = plt.figure(figsize=(25,20))
-        tree.plot_tree(classifier, feature_names=df.columns[0:-1]  ,class_names=list(set(y)) ,filled=True)
-        treeimg="static/images//"+db_choices[current_choice]+"-treeimg.jpg"
-        fig.savefig(treeimg)
-        return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="Decisiontree model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1],img=treeimg )
-    
-    elif model_choice == 'KNN':
-        from sklearn import neighbors
-        classifier=neighbors.KNeighborsClassifier()
-        classifier.fit(x,y)
-        pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-knn.pkl",'wb'))
-        with open("uploads//"+db_choices[current_choice]+"-knn.pkl", 'rb') as f:
-            data = pickle.load(f)
-        return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="K-NN model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
-    
-    elif model_choice == 'SVM':
-        from sklearn import svm
-        classifier= svm.SVC(gamma='scale')
-        classifier.fit(x,y)
-        pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-svm.pkl",'wb'))
-        with open("uploads//"+db_choices[current_choice]+"-svm.pkl", 'rb') as f:
-            data = pickle.load(f)
-        return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="SVM model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
-    
-    elif model_choice == 'LogisticRegression':
-        from sklearn.linear_model import LogisticRegression
-        classifier = LogisticRegression(multi_class='auto',solver='lbfgs')
-        classifier.fit(x, y)
-        pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-lr.pkl",'wb'))
-        with open("uploads//"+db_choices[current_choice]+"-lr.pkl", 'rb') as f:
-            data = pickle.load(f)
-        return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="Logistic Regression model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
-    
-    elif model_choice == 'RandomForest':
-        from sklearn.ensemble import RandomForestClassifier
-        classifier = RandomForestClassifier()
-        classifier.fit(x,y)
-        pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-rf.pkl",'wb'))
-        with open("uploads//"+db_choices[current_choice]+"-rf.pkl", 'rb') as f:
-            data = pickle.load(f)
-        return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="Random Forest model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
+    try:
+        if model_choice == 'decisiontree':
+            from sklearn import tree
+            classifier=tree.DecisionTreeClassifier()
+            classifier.fit(x,y)
+            pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-dtree.pkl",'wb'))
+            with open("uploads//"+db_choices[current_choice]+"-dtree.pkl", 'rb') as f:
+                data = pickle.load(f)
+            fig = plt.figure(figsize=(25,20))
+            tree.plot_tree(classifier, feature_names=df.columns[0:-1]  ,class_names=list(set(y)) ,filled=True)
+            treeimg="static/images//"+db_choices[current_choice]+"-treeimg.jpg"
+            fig.savefig(treeimg)
+            return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="Decisiontree model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1],img=treeimg )
+        
+        elif model_choice == 'KNN':
+            from sklearn import neighbors
+            classifier=neighbors.KNeighborsClassifier()
+            classifier.fit(x,y)
+            pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-knn.pkl",'wb'))
+            with open("uploads//"+db_choices[current_choice]+"-knn.pkl", 'rb') as f:
+                data = pickle.load(f)
+            return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="K-NN model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
+        
+        elif model_choice == 'SVM':
+            from sklearn import svm
+            classifier= svm.SVC(gamma='scale')
+            classifier.fit(x,y)
+            pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-svm.pkl",'wb'))
+            with open("uploads//"+db_choices[current_choice]+"-svm.pkl", 'rb') as f:
+                data = pickle.load(f)
+            return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="SVM model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
+        
+        elif model_choice == 'LogisticRegression':
+            from sklearn.linear_model import LogisticRegression
+            classifier = LogisticRegression(multi_class='auto',solver='lbfgs')
+            classifier.fit(x, y)
+            pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-lr.pkl",'wb'))
+            with open("uploads//"+db_choices[current_choice]+"-lr.pkl", 'rb') as f:
+                data = pickle.load(f)
+            return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="Logistic Regression model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
+        
+        elif model_choice == 'RandomForest':
+            from sklearn.ensemble import RandomForestClassifier
+            classifier = RandomForestClassifier()
+            classifier.fit(x,y)
+            pickle.dump(classifier, open("uploads//"+db_choices[current_choice]+"-rf.pkl",'wb'))
+            with open("uploads//"+db_choices[current_choice]+"-rf.pkl", 'rb') as f:
+                data = pickle.load(f)
+            return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="Random Forest model trained and stored in localstorage",data=data,model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] )
+        
+    except Exception as e:
+        print(e)
+        return render_template('index.html',info="Dataset has {} rows, {} columns".format(rows,cols),response="Training Unsuccessful, inappropriate CSV data",model_choice=model_choice, db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1],img="null" )
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -178,13 +183,10 @@ def dbchange():
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
@@ -192,13 +194,19 @@ def upload():
             filename = secure_filename(file.filename)
             df = pd.read_csv(file)
             name=filename.rsplit('.', 1)[0]
-            db_choices.append(name)
-            global current_choice
-            current_choice = db_choices.index(name)
-            df.to_pickle('uploads//'+name+'-data.pkl')
-            rows=len(df)
-            col=len(df.columns)
-            return render_template('index.html',info="Dataset has {r} rows, {c} columns".format(r=rows,c=col), db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] ,response="Database {} Upload Successful".format(name.upper()))
+            if name not in db_choices:
+                db_choices.append(name)
+                global current_choice
+                current_choice = db_choices.index(name)
+                df.to_pickle('uploads//'+name+'-data.pkl')
+                rows=len(df)
+                col=len(df.columns)
+                return render_template('index.html',info="Dataset has {r} rows, {c} columns".format(r=rows,c=col), db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] ,response="Database {} Upload Successful".format(name.upper()))
+            else:
+                df = pd.read_pickle("uploads//"+db_choices[current_choice]+"-data.pkl")
+                rows=len(df)
+                col=len(df.columns)
+                return render_template('index.html',info="Dataset has {r} rows, {c} columns".format(r=rows,c=col), db_choices = db_choices, current_choice = db_choices[current_choice], options = df.columns[0:-1] ,response="Database {} already exists".format(name.upper()))
 
 if __name__ == "__main__":
     app.run(debug=True)
